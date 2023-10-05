212d4ab494682c5fe7df68039aa72536b8d56b76
Morgana Ramos <morganaramos@edu.unifor.br>
2023-10-05 20:01:28 -0300

    criando routes para testes

--- server.js
+++ server.js
@@ -4,16 +4,31 @@ import {fastify} from "fastify";
 const server = fastify()
 
 const host = 'localhost' //127.0.0.1
-const port = '5001'
+const port = '5000'
 
 server.get('/', async (req, res) => {
     res.send('servidor no ar')
 })
 
 server.get('/produtos', async (req, res) => {
-    res.send('produtos')
+    res.send('mostrar produtos')
 })
 
+server.get('/produto/:id', async(req, res) =>{
+    res.send('mostrar produto por id')
+})
+
+server.post('/produto', async (req,res) => {
+    res.send('criando produto')
+})
+
+server.put('/produto/:id', async(req, res) =>{
+    res.send('alterando um produto por id')
+})
+
+server.delete('/produto/:id', async(req, res) => {
+    res.send('Deletando um produto por id')
+})
 
 server.listen({
     port,
